cbea3ff8ac59e2820694137ff61d9eca2726c32d
Lee-yeonkyu <dldusrb46@gmail.com>
2021-10-08 15:40:21 +0900

    commit

---
+++ src/10_08Project/ex3.java
@@ -0,0 +1,29 @@
+class Points {
+    private int x, y; // 한 점을 구성하는 x, y 좌표
+    public Points() {
+        this.x = this.y = 0;
+    }
+    public Points(int x, int y) {
+        this.x = x; this.y = y;
+    }
+    public void showPoint() { // 점의 좌표 출력
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
+class ColorPoints extends Points {
+    private String color; // 점의 색
+    public ColorPoints(int x, int y, String color) {
+        super(x, y); // Point의 생성자 Point(x, y) 호출
+        this.color = color;
+    }
+    public void showColorPoint() { // 컬러 점의 좌표 출력
+        System.out.print(color);
+        showPoint(); // Point 클래스의 showPoint() 호출
+    }
+}
+public class ex3 {
+    public static void main(String[] args) {
+        ColorPoints cp = new ColorPoints(5, 6, "blue");
+        cp.showColorPoint();
+    }
+}
